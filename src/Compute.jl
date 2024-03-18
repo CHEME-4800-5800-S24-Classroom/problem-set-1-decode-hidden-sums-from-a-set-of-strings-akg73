072167fab6897b02a36e0d4c35b5fa1c6f0e752e
Irongupta123 <akg73@cornell.edu>
2024-03-18 17:18:42 -0400

    Updated Compute.jl
    Added Documentation and implemented private method for part 1

--- src/Compute.jl
+++ src/Compute.jl
@@ -1,8 +1,40 @@
 # ===== PRIVATE METHODS BELOW HERE =================================================================================== #
 # TODO: Put private helper methods here. Don't forget to use the _ naming convention, and to add basic documentation.
+"""
+ _code_builder_part_1(model::MyPuzzleRecordModel) -> Int 64
 
+ Computes the value of the argument, which is a coded line. 
+
+ ### Arguements
+ - `model::MyPuzzleRecordModel`: object containing information for each line of the coded message.
+
+ ### Returns
+ - `::Int64`: value of decoded line
+
+"""
+function  _code_builder_part_1(model::MyPuzzleRecordModel)::Int64
+    
+    placeholder =model.characters
+    numberarray = filter(isdigit,placeholder)
+    line_sum = numberarray[1]*numberarray[end]
+    return parse(Int64,line_sum)
+end
+
+ """
+    _code_builder_part_2(model::MyPuzzleRecordModel) -> Int 64
+
+        Computes the value of the argument, which is a coded line, for the more complex rule.
+
+      ### Arguements
+       - `model::MyPuzzleRecordModel`: object containing information for each line of the coded message.
+
+      ### Returns
+      - `Int64`: value of decoded line
+
+
+"""
 #=
-function codes_builder_(lineNumber::Int64,total_string::String)
+function _codes_builder_part_2(model::MyPuzzleRecordModel())::Int64
     codes[lineNumber] = Int64(total_string)
     return codes
 end
@@ -15,19 +47,25 @@ end
 """
     decode_part_1(models::Dict{Int64, MyPuzzleRecordModel}) -> Tuple{Int64, Dict{Int64, Int64}}
 
-TODO: Add documentation
+        Cycles through each line of coded message stored in the 'models' Dictionary and returns the sum of message 
+        alongside dictionary of value per code
+     ### Arguements:
+          - ' models:: Dict{Int64, MyPuzzleRecordModel} ' : Contains each line of coded message 
+   
+        ### Return:
+          - 'Tuple{Int64, Dict{Int64, Int64} ' : Total sum and Dictionary of sum per line. 
+            Keys = code dictionary, values = sums
+ 
 """
 function decode_part_1(models::Dict{Int64, MyPuzzleRecordModel})::Tuple{Int64, Dict{Int64, Int64}}
     
     # initialize -
     total = 0;
     codes = Dict{Int64, Int64}();
-    for (lineNumber, model) in models  
-        placeholder =model.characters
-        numberarray = filter(isdigit,placeholder)
-        line_sum = numberarray[1]*numberarray[end]
-        codes[lineNumber] = parse(Int64,line_sum)
-        total+= parse(Int64,line_sum)
+    for (lineNumber, model) in models 
+        
+        codes[lineNumber] = _code_builder_part_1(model)
+        total+= codes[lineNumber]
     end   
     return (total,codes);
 end
@@ -69,8 +107,16 @@ end
     =#
 """
     decode_part_2(models::Dict{Int64, MyPuzzleRecordModel}) -> Tuple{Int64, Dict{Int64, Int64}}
+   
+        Cycles through each line of coded message stored in the 'models' Dictionary and returns the sum of message 
+        alongside dictionary of value per code. Operates on more complex input
 
-TODO: Add documentation
+    ### Arguements:
+          - ' models:: Dict{Int64, MyPuzzleRecordModel} ' : Contains each line of coded message 
+   
+    ### Return:
+          - 'Tuple{Int64, Dict{Int64, Int64} ' : Total sum and Dictionary of sum per line
+             Keys = code dictionary, values = sums
 """
 function decode_part_2(models::Dict{Int64, MyPuzzleRecordModel})::Tuple{Int64, Dict{Int64, Int64}}
      
@@ -80,6 +126,7 @@ function decode_part_2(models::Dict{Int64, MyPuzzleRecordModel})::Tuple{Int64, D
     for (lineNumber, model) in models  
         placeholder = model.characters 
         forward = replace([])
+    end
     # TODO: Add the logic for part 2 here
     # ...
      
